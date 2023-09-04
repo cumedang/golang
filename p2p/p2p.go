7503e5864456812d1819a4f9e052bc98ad9e7b83
cumedang <yusun2234sun@gmail.com>
2023-09-04 16:47:19 +0900

    p2p완료(recap)전

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,26 +3,30 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"time"
+	"strings"
 
 	"github.com/cumedang/go/utils"
 	"github.com/gorilla/websocket"
 )
 
-var upgrade = websocket.Upgrader{}
+var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
-	upgrade.CheckOrigin = func(r *http.Request) bool {
+	//port :3000이 port :4000에서 온 request를 upgrad합니다
+	upgrader.CheckOrigin = func(r *http.Request) bool {
 		return true
 	}
-	conn, err := upgrade.Upgrade(rw, r, nil)
+	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandelERror(err)
-	for {
-		_, p, err := conn.ReadMessage()
-		utils.HandelERror(err)
-		fmt.Printf("Just got:%s\n\n", p)
-		time.Sleep(5 * time.Second)
-		message := fmt.Sprintf("wa also think that: %s", p)
-		utils.HandelERror(conn.WriteMessage(websocket.TextMessage, []byte(message)))
-	}
+	openProt := r.URL.Query().Get("openPort")
+	result := strings.Split(r.RemoteAddr, ":")
+	initPeer(conn, result[0], openProt)
+
+}
+
+func AddoPeer(address, port, openPort string) {
+	//Porst :4000이 porst:3000과 연결하기를 원해요
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort), nil)
+	utils.HandelERror(err)
+	initPeer(conn, address, port)
 }
--- rest/rest.go
+++ rest/rest.go
@@ -47,6 +47,10 @@ type addTxPayload struct {
 	Amount int
 }
 
+type addPeerPayload struct {
+	Address, Port string
+}
+
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []urlDescription{
 		{
@@ -164,6 +168,20 @@ func transactions(rw http.ResponseWriter, r *http.Request) {
 func mywallet(rw http.ResponseWriter, r *http.Request) {
 	address := wallet.Wallet().Address
 	json.NewEncoder(rw).Encode(mywalletResponse{Address: address})
+
+}
+
+func peers(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		var payload addPeerPayload
+		json.NewDecoder(r.Body).Decode(&payload)
+		p2p.AddoPeer(payload.Address, payload.Port, port)
+		rw.WriteHeader(http.StatusOK)
+	case "GET":
+		json.NewEncoder(rw).Encode(p2p.Peers)
+	}
+
 }
 
 func Start(aPort int) {
@@ -179,6 +197,7 @@ func Start(aPort int) {
 	router.HandleFunc("/wallet", mywallet).Methods("GET")
 	router.HandleFunc("/transactions", transactions).Methods("POST")
 	router.HandleFunc("/ws", p2p.Upgrade).Methods("GET")
+	router.HandleFunc("/peers", peers).Methods("GET", "POST")
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, router))
 }
