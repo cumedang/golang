12f2c2f3990b78f279fe48654d51c10ad9861756
cumedang <yusun2234sun@gmail.com>
2023-09-04 17:24:07 +0900

    p2p recap완료

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,7 +3,6 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/cumedang/go/utils"
 	"github.com/gorilla/websocket"
@@ -12,21 +11,20 @@ import (
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
-	//port :3000이 port :4000에서 온 request를 upgrad합니다
+	openProt := r.URL.Query().Get("openPort")
+	ip := utils.Splitter(r.RemoteAddr, ":", 0)
 	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
+		return openProt != "" && ip != ""
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandelERror(err)
-	openProt := r.URL.Query().Get("openPort")
-	result := strings.Split(r.RemoteAddr, ":")
-	initPeer(conn, result[0], openProt)
+	initPeer(conn, ip, openProt)
 
 }
 
 func AddoPeer(address, port, openPort string) {
 	//Porst :4000이 porst:3000과 연결하기를 원해요
-	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort), nil)
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandelERror(err)
 	initPeer(conn, address, port)
 }
